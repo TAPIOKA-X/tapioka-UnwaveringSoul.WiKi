99f3301f7ef9368691fabd0bafa14d7cd2480c34
tapioka-jp <suetake0301@outlook.jp>
2025-05-18 20:51:21 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,8 @@
 //Link
-function MenuLink(){
+function HomeLink(){
   location.href= "https://unwaveringsoul.wiki.tapioka-site.f5.si/";
 }
 
+function MenuLink(){
+  location.href= "https://unwaveringsoul.wiki.tapioka-site.f5.si/Menu.html";
+}
